ef7fa787159092b7e73579f513398bbda55d9482
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-12-18 15:23:44 +0200

    fix: reworked filter with using MIUI

--- src/app/components/PostFilter/PostFilter.tsx
+++ src/app/components/PostFilter/PostFilter.tsx
@@ -1,6 +1,13 @@
 import { Category } from "@/app/types/Category";
 import "./PostFilter.scss";
 
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { TextField } from "@mui/material";
+
 type Props = {
   query: string;
   setQuery: (value: string) => void;
@@ -18,39 +25,49 @@ const PostFilter: React.FC<Props> = ({
     setQuery(e.target.value);
   };
 
-  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSelectChange = (e: SelectChangeEvent) => {
     setCategory(e.target.value as Category);
   };
 
   return (
-    <div className="filter">
-      <select
-        name="categories"
-        id="categories"
-        className="filter__categories"
+    <FormControl
+      className="filter"
+      sx={{ display: "flex", flexDirection: "row" }}
+    >
+      <InputLabel id="demo-select-small-label" color="warning">
+        Category
+      </InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
         value={category}
+        label="Category"
         onChange={handleSelectChange}
+        color="warning"
+        className="filter__categories"
       >
         {Object.values(Category).map((selectedType) => (
-          <option
+          <MenuItem
             key={selectedType}
             value={selectedType}
             className="filter__category"
           >
             {selectedType.charAt(0).toUpperCase() + selectedType.slice(1)}
-          </option>
+          </MenuItem>
         ))}
-      </select>
+      </Select>
 
-      <input
-        id="filter__query"
-        className="filter__query"
+      <TextField
+        id="outlined-basic"
+        label="Enter post title"
+        variant="outlined"
         type="text"
+        className="filter__query"
         value={query}
         onChange={handleQueryChange}
-        placeholder="Enter post title"
+        color="warning"
       />
-    </div>
+    </FormControl>
   );
 };
 
